f9f669bfe89c02fba55769905e04925632af8990
Hasegawa0801 <53552540+Hasegawa0801@users.noreply.github.com>
2019-08-01 16:05:04 +0900

    B

--- line.php
+++ line.php
@@ -10,15 +10,46 @@ $json_object = json_decode($json_string);
 $replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
 $message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
 $message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
+$message_id = $json_object->{"events"}[0]->{"message"}->{"id"};
  
 //メッセージタイプが「text」以外のときは何も返さず終了
-if($message_type != "text") exit;
+// if($message_type != "text") exit;
+
+//画像ファイルのバイナリ取得
+$ch = curl_init("https://api.line.me/v2/bot/message/".$message_id."/content");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+ 'Content-Type: application/json; charser=UTF-8',
+ 'Authorization: Bearer ' . $accessToken
+ ));
+$result = curl_exec($ch);
+curl_close($ch);
+
+//画像ファイルの作成  
+$fp = fopen('./img/test.jpg', 'wb');
+
+if ($fp){
+    if (flock($fp, LOCK_EX)){
+        if (fwrite($fp,  $result ) === FALSE){
+            print('ファイル書き込みに失敗しました<br>');
+        }else{
+            print($data.'をファイルに書き込みました<br>');
+        }
+
+        flock($fp, LOCK_UN);
+    }else{
+        print('ファイルロックに失敗しました<br>');
+    }
+}
+
+fclose($fp);
+
  
 //返信メッセージ
-$return_message_text = $message_text;
+// $return_message_text = $message_text;
  
 //返信実行
-sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
+// sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
 ?>
 <?php
 //メッセージの送信
