a587829cdfdbc9db2a18922b6db78759f7d3d943
Hasegawa0801 <53552540+Hasegawa0801@users.noreply.github.com>
2019-08-01 21:23:40 +0900

    a

--- line.php
+++ line.php
@@ -2,6 +2,12 @@
 
 define('DEBUG', './debug.txt');
 
-$input = file_get_contents('php://input');
+// 送られて来たJSONデータを取得
+$json_string = file_get_contents('php://input');
+$json = json_decode($json_string);
+// JSONデータから返信先を取得
+$replyToken = $json->events[0]->replyToken;
+// JSONデータから送られてきたメッセージを取得
+$message = $json->events[0]->message->text;
 
-file_put_contents(DEBUG, $input);
+file_put_contents(DEBUG, $replyToken.$message);
