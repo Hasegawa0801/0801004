b1465e8b0d7b889e4cf9d8290ee0c9a256256c4e
Hasegawa0801 <53552540+Hasegawa0801@users.noreply.github.com>
2019-08-01 21:18:54 +0900

    a

--- line.php
+++ line.php
@@ -1,40 +1,7 @@
 <?php
-// 送られて来たJSONデータを取得
-$json_string = file_get_contents('php://input');
-$json = json_decode($json_string);
-// JSONデータから返信先を取得
-$replyToken = $json->events[0]->replyToken;
-// JSONデータから送られてきたメッセージを取得
 
-// HTTPヘッダを設定
-$channelToken = 'ScsOQvzkti43X3SBf2XcpZvZAVDM/mHaNHrAOJfr+5eu7Gz8BoxQMljmW0y5NHpazyckMfKUETP53ZccURy7OjMcD7cIJNFhjCx4gM+pFGx3Vip4+j436klae2PR9MlQYD4uHuCsunVINo/cg/sa4QdB04t89/1O/w1cDnyilFU=';
-$headers = [
-	'Authorization: Bearer ' . $channelToken,
-	'Content-Type: application/json; charset=utf-8',
-];
+define('DEBUG', './debug.txt');
 
-// POSTデータを設定してJSONにエンコード
-$post = [
-	'replyToken' => $replyToken,
-	'messages' => [
-		[
-			'type' => 'text',
-			'text' => '「」',
-		],
-	],
-];
-$post = json_encode($post);
+$input = file_get_contents('php//input');
 
-// HTTPリクエストを設定
-$ch = curl_init('https://api.line.me/v2/bot/message/reply');
-$options = [
-	CURLOPT_CUSTOMREQUEST => 'POST',
-	CURLOPT_HTTPHEADER => $headers,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_BINARYTRANSFER => true,
-	CURLOPT_HEADER => true,
-	CURLOPT_POSTFIELDS => $post,
-];
-
-// 実行
-curl_setopt_array($ch, $options);
+file_put_contents(DEBUG, $input);
